088eba7686cb873fced860ac51c52828eccb80ee
Luka Tavkhelidze <52665503+Christus5@users.noreply.github.com>
2020-12-28 14:04:45 +0400

    creating Routes | MenuBar

---
+++ src/components/MenuBar/MenuBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './MenuBar.style.css';
+
+
+const MenuBar = () => {
+    return (
+        <nav className='menubar'>
+            <h1>Users API</h1>
+        </nav>
+    );
+};
+
+export default MenuBar;
